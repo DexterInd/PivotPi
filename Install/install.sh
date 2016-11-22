8e2a0e3d02fbc339028f4479ee32fa81ca2849bb
cleoqc <cleoqc1124@gmail.com>
2016-11-22 21:51:17 +0000

    change some of the messages on the screen

--- Install/install.sh
+++ Install/install.sh
@@ -27,32 +27,24 @@ then
 fi
 
 
-echo "Welcome to PivotPi Installer."
+
 echo "  ____  _            _   ____  _ "
 echo " |  _ \(_)_   _____ | |_|  _ \(_)"
 echo " | |_) | \ \ / / _ \| __| |_) | |"
 echo " |  __/| |\ V / (_) | |_|  __/| |"
 echo " |_|   |_| \_/ \___/ \__|_|   |_|"
 echo ""
+
+echo "Welcome to PivotPi Installer."
 			
 pushd /home/pi/Dexter/PivotPi/Software/Python
-sudo python setup.py
-sudo python3 setup.py
+sudo python setup.py install
+sudo python3 setup.py install
 popd					 
 
 if [[ "$quiet_mode" -eq 0 ]]
 then
-	echo " "
-	echo "Please restart the Raspberry Pi for the changes to take effect"
-	echo " "
-	echo "Please restart to implement changes!"
-	echo "  _____  ______  _____ _______       _____ _______ "
-	echo " |  __ \|  ____|/ ____|__   __|/\   |  __ \__   __|"
-	echo " | |__) | |__  | (___    | |  /  \  | |__) | | |   "
-	echo " |  _  /|  __|  \___ \   | | / /\ \ |  _  /  | |   "
-	echo " | | \ \| |____ ____) |  | |/ ____ \| | \ \  | |   "
-	echo " |_|  \_\______|_____/   |_/_/    \_\_|  \_\ |_|   "
-	echo " "
-	echo "Please restart to implement changes!"
-	echo "To Restart type sudo reboot"
+    echo " "
+    echo "Installation all done"
+    echo "Enjoy your PivotPi!"
 fi
