a8c50328103bad36c2754654a9375a1997961c61
Dexter Industries <dxtrindustries@gmail.com>
2016-12-06 13:21:23 +0400

    Update elf.py

--- Projects/Elf-on-a-Shelf/elf.py
+++ Projects/Elf-on-a-Shelf/elf.py
@@ -25,27 +25,20 @@ ultrasonic_ranger = 3
 # angle(channel, degrees) will set a servo position to roughly the degrees you specify. However
 # each servo is different, so if it's calibrated for one servo, it won't be the same for others.
 #
-# angle_microseconds(channel, us) will set servo's pulse width in microseconds. Setting this to 0
-# will disable the servo (let it float). 1500 is about center, and the range will depend on the servo.
-# Typically about 750 to 2250 is safe. If you set it to a number outside of the servo range, the servo
-# will try to fight it's mechanical stops, leaing to hot or burned out servos and increased battery load.
-# If this is acting differently between different PivotPi boards, try adjusting the calibation frequency
-# when you initialize the controller (i.e. when you call "pivotpi = PivotPi.servo(0x40, 60)").
-#
 # led(channel, percent) will set the LED brightness in percent (0-100). Using a floating point
 # percent value will increase the precision if necessary (e.g. 21.2 or 0.075). The PWM scale is 12-bit + 1
 # so the 0-100 percent range is divided into 4096 values (about 0.025% per value).
 
 # Initialise the PivotPi servo controller with the right address (0x40 if both switches are low), and the frequency
-# that it's actually running at. You can use 60 (target frequency), but in reality it can be off by more than 5%.
-# You can leave these two parameters empty to default to address 0x40 and the target frequency of 60Hz.
+# that it's actually running at. You can use 60 (target frequency).
+
 try:
     pivotpi = pivotpi.PivotPi(0x40, 60)
 except:
     print("PivotPi not found - quitting")
     exit(-1)
 
-print('Moving servos on channel 1-8, press Ctrl-C to quit...')
+print('The Elf is Live! Press Ctrl-C to quit...')
 
 while True:
     try:
@@ -54,12 +47,12 @@ while True:
 		
 		if distance < 50:
 				print("Distance: " + str(distance))
-				pivotpi.angle(0, 160)
-				pivotpi.led(0, 0)
+				pivotpi.angle(0, 160)		# Set the angle to 160.
+				pivotpi.led(0, 0)		# Set the LED power to 0%.
 				time.sleep(0.5)
 
-				pivotpi.angle(0, 0)
-				pivotpi.led(0, 100)
+				pivotpi.angle(0, 0)		# Set the angle to 0.
+				pivotpi.led(0, 100)		# Set the LED power to 100%.
 				time.sleep(0.5)
 		else:
 			print("Distance: " + str(distance))
